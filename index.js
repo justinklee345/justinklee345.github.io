094e80bf121e8580d18e345f48e1d5db691478f9
justinklee345 <72401945+justinklee345@users.noreply.github.com>
2023-08-18 00:53:53 +0900

    UI finished

---
+++ index.js
@@ -0,0 +1,28 @@
+function handleKeyDown(e) {
+    if (e.key == "Tab") {
+      e.preventDefault();
+
+      const start = this.selectionStart;
+      const end = this.selectionEnd;
+
+      this.value =
+        this.value.substring(0, start) + "\t" + this.value.substring(end);
+
+      this.selectionStart = this.selectionEnd = start + 1;
+    }
+}
+
+async function formatCode() {
+    console.log("well hello there?")
+    const response = await fetch("https://dog.ceo/api/breeds/image/random");
+    const formatted_code = await response.json()
+    console.log(formatted_code)
+
+    var inputCode = document.getElementById("inputCode").value;
+    console.log(inputCode)
+
+    var selected_language = document.querySelector('input[name="btnradio"]:checked').value;
+    console.log(selected_language)
+
+    document.getElementById("outputCode").value = formatted_code.message;
+}
